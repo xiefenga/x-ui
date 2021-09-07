233a46e6ef71237377aefcc884f1fe903391148b
xiefeng <xiefeng@deMacBook-Pro.local>
2021-09-07 16:27:24 +0800

    📝docs: 更新 api

--- src/components/Button/api/Button.tsx
+++ src/components/Button/api/Button.tsx
@@ -2,57 +2,50 @@ interface ButtonBaseProps {
   /**
    * 是否禁用
    */
-  disabled: boolean;
+  disabled: boolean
   /**
    * 按钮尺寸
    */
-  size: 'small' | 'large' | 'middle';
+  size: 'small' | 'large' | 'middle'
   /**
    * 按钮类型
    */
-  type:
-    | 'default'
-    | 'primary'
-    | 'link'
-    | 'success'
-    | 'info'
-    | 'warning'
-    | 'danger';
+  type: 'default' | 'primary' | 'link' | 'success' | 'info' | 'warning' | 'danger'
 
   /**
    * 按钮的链接
    */
-  href: string;
+  href: string
 
   /**
    * Anchor 的 target 属性，仅当 href 属性存在时有效
    */
-  target: string;
+  target: string
 
   /**
    * 原生 button 的 type 属性
    */
-  htmlType: 'submit' | 'button' | 'reset';
+  htmlType: 'submit' | 'button' | 'reset'
 
   /**
    * 按钮独占一行
    */
-  block: boolean;
+  block: boolean
   /**
    * 按钮形状
    */
-  shape: 'circle' | 'round';
+  shape: 'circle' | 'round'
   /**
    * 按钮的加载状态
    */
-  loading: boolean;
+  loading: boolean
   /**
    * 点击事件
    */
-  onClick: React.MouseEventHandler<HTMLElement>;
+  onClick: React.MouseEvent<HTMLButtonElement>
 }
 
-export type ButtonProps = Partial<ButtonBaseProps>;
+export type ButtonProps = Partial<ButtonBaseProps>
 
 const Button: React.FC<ButtonProps> = () => null
 
